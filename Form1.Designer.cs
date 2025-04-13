1e7b43290c080a2007e71572f4a9b2adfe431b0b
Coloured <79119198+Coloured568@users.noreply.github.com>
2025-04-13 14:24:09 -0400

    initial release

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace cmusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.load = new System.Windows.Forms.Button();
+            this.play = new System.Windows.Forms.Button();
+            this.skip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.volumeSlider1 = new NAudio.Gui.VolumeSlider();
+            this.songlength = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pause = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.songlength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.load.Location = new System.Drawing.Point(286, 221);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(75, 50);
+            this.load.TabIndex = 0;
+            this.load.Text = "Load playlist";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // play
+            // 
+            this.play.Font = new System.Drawing.Font("MS Gothic", 6.25F, System.Drawing.FontStyle.Bold);
+            this.play.Location = new System.Drawing.Point(367, 221);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 50);
+            this.play.TabIndex = 1;
+            this.play.Text = "Start/Stop";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // skip
+            // 
+            this.skip.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skip.Location = new System.Drawing.Point(531, 221);
+            this.skip.Name = "skip";
+            this.skip.Size = new System.Drawing.Size(75, 50);
+            this.skip.TabIndex = 2;
+            this.skip.Text = "Skip";
+            this.skip.UseVisualStyleBackColor = true;
+            this.skip.Click += new System.EventHandler(this.skip_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Now playing: Nothing";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(230, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 160);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(332, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "CMusic";
+            // 
+            // volumeSlider1
+            // 
+            this.volumeSlider1.Location = new System.Drawing.Point(336, 402);
+            this.volumeSlider1.Name = "volumeSlider1";
+            this.volumeSlider1.Size = new System.Drawing.Size(104, 14);
+            this.volumeSlider1.TabIndex = 7;
+            // 
+            // songlength
+            // 
+            this.songlength.Location = new System.Drawing.Point(230, 354);
+            this.songlength.Name = "songlength";
+            this.songlength.Size = new System.Drawing.Size(320, 42);
+            this.songlength.TabIndex = 9;
+            this.songlength.Scroll += new System.EventHandler(this.songlength_Scroll);
+            this.songlength.MouseDown += new System.Windows.Forms.MouseEventHandler(this.songlength_MouseDown);
+            this.songlength.MouseUp += new System.Windows.Forms.MouseEventHandler(this.songlength_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(363, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "0:00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pause
+            // 
+            this.pause.Font = new System.Drawing.Font("MS Gothic", 6.25F, System.Drawing.FontStyle.Bold);
+            this.pause.Location = new System.Drawing.Point(448, 221);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(75, 50);
+            this.pause.TabIndex = 11;
+            this.pause.Text = "Play/Pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(205, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.songlength);
+            this.Controls.Add(this.volumeSlider1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.skip);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.load);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.songlength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button skip;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private NAudio.Gui.VolumeSlider volumeSlider1;
+        private System.Windows.Forms.TrackBar songlength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace cmusic
+{
+    public partial class Form1 : Form
+    {
+        private List<string> playlist = new List<string>();
+        private int currentTrackIndex = 0;
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private bool isScrubbing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "CMusic";
+            string path = "config.txt";
+            string[] lines;
+
+            if (File.Exists(path))
+            {
+                lines = File.ReadAllLines(path);
+            }
+            else
+            {
+                lines = new string[]
+                {
+                    "BACKGROUND=BLACK",
+                    "COLOR=WHITE"
+                };
+                File.WriteAllLines(path, lines);
+            }
+
+            // Parse config and apply colors
+            Dictionary<string, string> config = new Dictionary<string, string>();
+            foreach (var line in lines)
+            {
+                var parts = line.Split('=');
+                if (parts.Length == 2)
+                {
+                    config[parts[0].Trim().ToUpper()] = parts[1].Trim().ToUpper();
+                }
+            }
+
+            if (config.ContainsKey("BACKGROUND"))
+                this.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.button1.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.play.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.pause.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.skip.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.load.BackColor = Color.FromName(config["BACKGROUND"]);
+                this.listBox1.BackColor = Color.FromName(config["BACKGROUND"]);
+
+            if (config.ContainsKey("COLOR"))
+            {
+                this.ForeColor = Color.FromName(config["COLOR"]);
+                ApplyColorsToControls(this.Controls, this.ForeColor);
+            }
+
+
+            this.Resize += Form1_Resize;
+            CenterLabel();
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            timer1.Interval = 100;
+            timer1.Tick += timer1_Tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "Audio Files|*.mp3;*.wav;*.aac;*.wma";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                playlist.Clear();
+                playlist.AddRange(ofd.FileNames);
+                currentTrackIndex = 0;
+
+                listBox1.Items.Clear();
+                foreach (var file in playlist)
+                {
+                    listBox1.Items.Add(Path.GetFileName(file));
+                }
+
+                MessageBox.Show("Playlist loaded with " + playlist.Count + " tracks.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (playlist.Count == 0)
+            {
+                MessageBox.Show("No playlist loaded.");
+                return;
+            }
+
+            PlayCurrentTrack();
+            UpdateLabel();
+        }
+
+        private void skip_Click(object sender, EventArgs e)
+        {
+            currentTrackIndex++;
+            if (currentTrackIndex < playlist.Count)
+            {
+                PlayCurrentTrack();
+                UpdateLabel();
+            }
+            else
+            {
+                MessageBox.Show("End of playlist.");
+                currentTrackIndex = 0;
+            }
+        }
+
+        private void previous_Click(object sender, EventArgs e)
+        {
+            currentTrackIndex--;
+            if (currentTrackIndex < playlist.Count && currentTrackIndex >= 0)
+            {
+                PlayCurrentTrack();
+                UpdateLabel();
+            }
+            else
+            {
+                MessageBox.Show("Start of playlist.");
+                currentTrackIndex = 0;
+            }
+        }
+
+        private void PlayCurrentTrack()
+        {
+            try
+            {
+                if (outputDevice != null)
+                {
+                    if (outputDevice.PlaybackState == PlaybackState.Playing)
+                        outputDevice.Stop();
+
+                    outputDevice.PlaybackStopped -= OnPlaybackStopped;
+                    outputDevice.Dispose();
+                    outputDevice = null;
+                }
+
+                if (audioFile != null)
+                {
+                    audioFile.Dispose();
+                    audioFile = null;
+                }
+
+                audioFile = new AudioFileReader(playlist[currentTrackIndex]);
+                outputDevice = new WaveOutEvent();
+                outputDevice.Init(audioFile);
+                songlength.Value = 0;
+                songlength.Maximum = (int)Math.Max(1, audioFile.TotalTime.TotalSeconds);
+                outputDevice.Play();
+                timer1.Start();
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error playing file: " + ex.Message);
+            }
+        }
+
+        private void OnPlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Stopped)
+            {
+                currentTrackIndex++;
+                if (currentTrackIndex < playlist.Count)
+                {
+                    PlayCurrentTrack();
+                    UpdateLabel();
+                }
+                else
+                {
+                    timer1.Stop();
+                }
+            }
+        }
+
+        private void UpdateLabel()
+        {
+            if (currentTrackIndex < playlist.Count)
+            {
+                string fileName = Path.GetFileName(playlist[currentTrackIndex]);
+                label1.Text = $"Now playing: {fileName}";
+                CenterLabel();
+
+                if (currentTrackIndex >= 0 && currentTrackIndex < listBox1.Items.Count)
+                {
+                    listBox1.SelectedIndex = currentTrackIndex;
+                }
+            }
+        }
+
+        private void CenterLabel()
+        {
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label2.Left = (this.ClientSize.Width - label2.Width) / 2;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            CenterLabel();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (outputDevice != null)
+            {
+                outputDevice.Stop();
+                outputDevice.Dispose();
+            }
+
+            if (audioFile != null)
+            {
+                audioFile.Dispose();
+            }
+        }
+
+        private TimeSpan GetTimeRemaining()
+        {
+            if (audioFile != null)
+            {
+                return audioFile.TotalTime - audioFile.CurrentTime;
+            }
+            return TimeSpan.Zero;
+        }
+
+        private void UpdateTimeDisplay()
+        {
+            if (audioFile != null)
+            {
+                TimeSpan remaining = GetTimeRemaining();
+                label3.Text = $"{remaining:mm\\:ss}";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1 && listBox1.SelectedIndex < playlist.Count)
+            {
+                currentTrackIndex = listBox1.SelectedIndex;
+                PlayCurrentTrack();
+                UpdateLabel();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (audioFile != null && !isScrubbing)
+            {
+                if (audioFile.TotalTime.TotalSeconds > 0)
+                {
+                    songlength.Value = Math.Min(songlength.Maximum,
+                        (int)(audioFile.CurrentTime.TotalSeconds));
+                }
+
+                UpdateTimeDisplay();
+            }
+        }
+
+        private void songlength_MouseDown(object sender, MouseEventArgs e)
+        {
+            isScrubbing = true;
+        }
+
+        private void songlength_MouseUp(object sender, MouseEventArgs e)
+        {
+            UpdatePlaybackPosition();
+            isScrubbing = false;
+            UpdateTimeDisplay();
+        }
+
+        private void songlength_Scroll(object sender, EventArgs e)
+        {
+            if (audioFile != null && audioFile.TotalTime.TotalSeconds > 0)
+            {
+                double targetSeconds = songlength.Value;
+                TimeSpan remaining = audioFile.TotalTime - TimeSpan.FromSeconds(targetSeconds);
+                label3.Text = $"{remaining:mm\\:ss}";
+            }
+        }
+
+        private void UpdatePlaybackPosition()
+        {
+            if (audioFile != null && audioFile.TotalTime.TotalSeconds > 0)
+            {
+                double targetSeconds = songlength.Value;
+                audioFile.CurrentTime = TimeSpan.FromSeconds(targetSeconds);
+            }
+        }
+
+        private void pause_click(object sender, EventArgs e)
+        {
+            if (outputDevice.PlaybackState == PlaybackState.Paused)
+            {
+                outputDevice.Play();
+            }
+            else
+            {
+                outputDevice.Pause();
+            }
+        }
+
+        private void ApplyColorsToControls(Control.ControlCollection controls, Color foreColor)
+        {
+            foreach (Control ctrl in controls)
+            {
+                ctrl.ForeColor = foreColor;
+
+                // Optionally update background color too
+                // ctrl.BackColor = this.BackColor;
+
+                if (ctrl.HasChildren)
+                {
+                    ApplyColorsToControls(ctrl.Controls, foreColor);
+                }
+            }
+        }
+
+    }
+}
