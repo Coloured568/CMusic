92917a314de1a8cdf415297680d405fa2ed9e64e
Coloured <79119198+Coloured568@users.noreply.github.com>
2025-04-15 23:11:04 -0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,11 @@ namespace cmusic
             this.pause = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.visualizerBox = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.loop = new System.Windows.Forms.CheckBox();
+            this.shuffle = new System.Windows.Forms.CheckBox();
+            this.loop_single = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.songlength)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.visualizerBox)).BeginInit();
             this.SuspendLayout();
@@ -51,18 +56,18 @@ namespace cmusic
             // load
             // 
             this.load.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.load.Location = new System.Drawing.Point(286, 221);
+            this.load.Location = new System.Drawing.Point(612, 45);
             this.load.Name = "load";
             this.load.Size = new System.Drawing.Size(75, 50);
             this.load.TabIndex = 0;
-            this.load.Text = "Load playlist";
+            this.load.Text = "Create Playlist";
             this.load.UseVisualStyleBackColor = true;
             this.load.Click += new System.EventHandler(this.button1_Click);
             // 
             // play
             // 
             this.play.Font = new System.Drawing.Font("MS Gothic", 6.25F, System.Drawing.FontStyle.Bold);
-            this.play.Location = new System.Drawing.Point(367, 221);
+            this.play.Location = new System.Drawing.Point(313, 231);
             this.play.Name = "play";
             this.play.Size = new System.Drawing.Size(75, 50);
             this.play.TabIndex = 1;
@@ -73,7 +78,7 @@ namespace cmusic
             // skip
             // 
             this.skip.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.skip.Location = new System.Drawing.Point(531, 221);
+            this.skip.Location = new System.Drawing.Point(475, 230);
             this.skip.Name = "skip";
             this.skip.Size = new System.Drawing.Size(75, 50);
             this.skip.TabIndex = 2;
@@ -144,7 +149,7 @@ namespace cmusic
             // pause
             // 
             this.pause.Font = new System.Drawing.Font("MS Gothic", 6.25F, System.Drawing.FontStyle.Bold);
-            this.pause.Location = new System.Drawing.Point(448, 221);
+            this.pause.Location = new System.Drawing.Point(394, 231);
             this.pause.Name = "pause";
             this.pause.Size = new System.Drawing.Size(75, 50);
             this.pause.TabIndex = 11;
@@ -155,7 +160,7 @@ namespace cmusic
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(205, 221);
+            this.button1.Location = new System.Drawing.Point(232, 230);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 50);
             this.button1.TabIndex = 12;
@@ -172,12 +177,69 @@ namespace cmusic
             this.visualizerBox.TabStop = false;
             this.visualizerBox.Click += new System.EventHandler(this.visualizerBox_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(612, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Load playlist";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.loadplaylist_click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MS Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(612, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Save playlist";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.saveplaylist_click);
+            // 
+            // loop
+            // 
+            this.loop.AutoSize = true;
+            this.loop.Location = new System.Drawing.Point(612, 213);
+            this.loop.Name = "loop";
+            this.loop.Size = new System.Drawing.Size(50, 17);
+            this.loop.TabIndex = 16;
+            this.loop.Text = "Loop";
+            this.loop.UseVisualStyleBackColor = true;
+            // 
+            // shuffle
+            // 
+            this.shuffle.AutoSize = true;
+            this.shuffle.Location = new System.Drawing.Point(612, 263);
+            this.shuffle.Name = "shuffle";
+            this.shuffle.Size = new System.Drawing.Size(59, 17);
+            this.shuffle.TabIndex = 17;
+            this.shuffle.Text = "Shuffle";
+            this.shuffle.UseVisualStyleBackColor = true;
+            // 
+            // loop_single
+            // 
+            this.loop_single.AutoSize = true;
+            this.loop_single.Location = new System.Drawing.Point(612, 236);
+            this.loop_single.Name = "loop_single";
+            this.loop_single.Size = new System.Drawing.Size(82, 17);
+            this.loop_single.TabIndex = 18;
+            this.loop_single.Text = "Loop Single";
+            this.loop_single.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loop_single);
+            this.Controls.Add(this.shuffle);
+            this.Controls.Add(this.loop);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.visualizerBox);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pause);
@@ -225,6 +287,11 @@ namespace cmusic
         private System.Windows.Forms.Button pause;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox visualizerBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox loop;
+        private System.Windows.Forms.CheckBox shuffle;
+        private System.Windows.Forms.CheckBox loop_single;
     }
 }
 
